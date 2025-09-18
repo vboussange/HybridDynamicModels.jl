2cfd01c15b904031695b82536931838c6b378a65
Victor Boussange <vic.boussange@gmail.com>
2025-09-18 16:42:02 +0200

    added tests for external dependencies

--- test/layers/utilities/parameter.jl
+++ test/layers/utilities/parameter.jl
@@ -1,28 +1,24 @@
 @testset "ParameterLayer" begin
     rng = StableRNG(42)
+    init_value = (; a = ones(3), b = randn(3))
 
     # no constraint
     param = ParameterLayer(; constraint = NoConstraint(),
-        init_value = (; u0 = ones(10)))
+        init_value)
 
-    ps, st = Lux.setup(Random.default_rng(), param)
+    ps, st = LuxCore.setup(rng, param)
     kwargs, _ = param(ps, st)
-    @test all(kwargs.u0 .≈ ones(10))
+    @test all(kwargs.a == ones(3))
 
     # test with a constraint
     constraint = NamedTupleConstraint((;
-        a = BoxConstraint(0.0, 3.0))
+        a = BoxConstraint([0.0], [3.0]))
     )
-    param = ParameterLayer(; constraint, init_value = (; a = ones(3), b = randn(3)))
+    param = ParameterLayer(; constraint, init_value)
 
-    ps, st = Lux.setup(Random.default_rng(), param)
-    @test !any(ps.a .≈ ones(3)) # should be transformed
+    ps, st = LuxCore.setup(rng, param)
+    @test !any(ps.a .== ones(3)) # should be transformed
     kwargs, _ = param(ps, st)
-    @test all(kwargs.a .≈ ones(3))
+    @test all(kwargs.a .== ones(3))
 
-    # gradient
-    fun = ps -> sum(param(ps, st)[1].a)
-    grad = value_and_gradient(fun, AutoZygote(), ps)[2]
-    @test !isnothing(grad.a)
-    @test isnothing(grad.b)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,18 +4,18 @@ using Random
 using StableRNGs
 using DifferentiationInterface
 using Zygote
+using LuxCore
 
 
 @testset "HybridDynamicModels.jl" begin
     include("qa.jl")
-    
+
     include("data.jl")
     include("constraints.jl")
     
     # Layer tests
     @testset "Layer Utilities" begin
         include("layers/utilities/parameter.jl")
-        include("layers/utilities/bayesian_layer.jl")
     end
     
     @testset "Model Layers" begin
@@ -25,10 +25,11 @@ using Zygote
     end
     
     # Training API tests
-    @testset "Training API" begin
-        include("train/generics.jl")
-        include("train/lux_trainer.jl")
-        include("train/mcmc_trainer.jl")
-        include("train/vi_trainer.jl")
+    @testset "HybridDynamicModelsLuxExt" begin
+        include("ext/HybridDynamicModelsLuxExt.jl")
+    end
+
+    @testset "HybridDynamicModelsTuringExt" begin
+        include("ext/HybridDynamicModelsTuringExt.jl")
     end
 end
